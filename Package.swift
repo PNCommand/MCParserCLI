59034193b0185caad022d110322bad21725011b4
yechentide <yechentide@gmail.com>
2022-08-13 14:02:28 +0900

    init

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MCParserCLI",
+    platforms: [
+        .macOS(.v11),
+        .iOS(.v11)
+    ],
+    products: [
+        .executable(name: "mcp", targets: ["MCParserCLI"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.2"),
+        .package(url: "https://github.com/yechentide/CoreBedrock", exact: "1.0.4")
+    ],
+    targets: [
+        .executableTarget(
+            name: "MCParserCLI",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "CoreBedrock", package: "CoreBedrock")
+            ]),
+        // .testTarget(
+        //     name: "MCParserCLITests",
+        //     dependencies: ["MCParserCLI"]),
+    ]
+)
